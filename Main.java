dd7a27fae2bed9e6c66192815209f6b4d4a660e7
nehadalvi <ndalvi1@uncc.edu>
2016-10-05 21:46:59 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		MySQLAccess dao=new MySQLAccess();
+		dao.readDataBase();
+
+	}
+
+}
